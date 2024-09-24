de7bc5684906e43cf456b63746c2ede8795dad8d
Roman <romamastakov@yandex.ru>
2024-09-24 13:29:16 +0300

    Update login.cy.js
    Deleted info pass & login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,8 +4,8 @@ describe('Проверка авторизации', function () {
          cy.visit('https://login.qa.studio/'); //зашел на сайт
          cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверка цвета кнопки восст.пар
 
-         cy.get('#mail').type('german@dolnikov.ru'); //ввел верный логин
-         cy.get('#pass').type('iLoveqastudio1'); //ввел верный пароль
+         cy.get('#mail').type('usermail@e-mail.ru'); //ввел верный логин
+         cy.get('#pass').type('passwordcorrect'); //ввел верный пароль
          cy.get('#loginButton').click(); //нажал войти
 
          cy.get('#messageHeader').contains('Авторизация прошла успешно'); //Проверка после авторизации видно текст
@@ -16,8 +16,8 @@ describe('Проверка авторизации', function () {
     cy.visit('https://login.qa.studio/'); //зашел на сайт
     cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверка цвета кнопки восст.пар
 
-    cy.get('#mail').type('german@dolnikov.ru'); //ввел верный логин
-    cy.get('#pass').type('iLovestudio1'); //ввел не верный пароль
+    cy.get('#mail').type('usermail@e-mail.ru'); //ввел верный логин
+    cy.get('#pass').type('passworduncorrect'); //ввел не верный пароль
     cy.get('#loginButton').click(); //нажал войти
 
     cy.get('#messageHeader').contains('Такого логина или пароля нет'); //Проверка после авторизации видно текст
@@ -28,8 +28,8 @@ describe('Проверка авторизации', function () {
     cy.visit('https://login.qa.studio/'); //зашел на сайт
     cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверка цвета кнопки восст.пар
 
-    cy.get('#mail').type('germas@dolnikov.ru'); //ввел не верный логин
-    cy.get('#pass').type('iLovestudio1'); //ввел верный пароль
+    cy.get('#mail').type('usermail@e-mail.ru'); //ввел не верный логин
+    cy.get('#pass').type('passwordcorrect'); //ввел верный пароль
     cy.get('#loginButton').click(); //нажал войти
 
     cy.get('#messageHeader').contains('Такого логина или пароля нет'); //Проверка после авторизации видно текст
@@ -40,8 +40,8 @@ describe('Проверка авторизации', function () {
     cy.visit('https://login.qa.studio/'); //зашел на сайт
     cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверка цвета кнопки восст.пар
 
-    cy.get('#mail').type('germandolnikov.ru'); //ввел верный логин, без @
-    cy.get('#pass').type('iLoveqastudio1'); //ввел верный пароль
+    cy.get('#mail').type('usermaile-mail.ru'); //ввел верный логин, без @
+    cy.get('#pass').type('passwordcorrect'); //ввел верный пароль
     cy.get('#loginButton').click(); //нажал войти
 
     cy.get('#messageHeader').contains('Нужно исправить проблему валидации'); //Проверка после авторизации видно текст
@@ -52,12 +52,12 @@ describe('Проверка авторизации', function () {
     cy.visit('https://login.qa.studio/'); //зашел на сайт
     cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверка цвета кнопки восст.пар
 
-    cy.get('#mail').type('GerMan@Dolnikov.ru');
-    cy.get('#pass').type('iLoveqastudio1');
+    cy.get('#mail').type('usErmaIl@e-mail.ru'); Ввел верный логин, с заглавными буквами
+    cy.get('#pass').type('passwordcorrect'); //Ввел верный пароль
     cy.get('#loginButton').click(); //нажал войти
 
     cy.get('#messageHeader').should('be.visible');
-    cy.get('#messageHeader').contains('Авторизация прошла успешно')
+    cy.get('#messageHeader').contains('Авторизация прошла успешно') 
     cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //Виден крестик пол-ю
     })
  it('Проверка восстановления пароля', function () {
@@ -66,7 +66,7 @@ describe('Проверка авторизации', function () {
 
     cy.get('#forgotEmailButton').click(); //нажал восстановить пароль
 
-    cy.get('#mailForgot').type('german@dolnikov.ru'); //Ввел почту для восстановления
+    cy.get('#mailForgot').type('usermail@e-mail.ru'); //Ввел почту для восстановления
     cy.get('#restoreEmailButton').click(); // Нажал отправить код
 
     cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail'); //Проверка на совпадение текст
